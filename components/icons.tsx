b0c10ca711dbdf0169a7a7ef6362fe4c0b1ebbcf
foodgramz <29430374@qq.com>
2025-05-28 21:58:46 +0800

    fix: add className support to logo icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,9 +1,12 @@
 import { Loader2, Github } from "lucide-react"
+import { cn } from "@/lib/utils"
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {}
 
 export const Icons = {
   spinner: Loader2,
   gitHub: Github,
-  logo: () => (
+  logo: ({ className, ...props }: IconProps) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
@@ -12,7 +15,8 @@ export const Icons = {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="h-6 w-6"
+      className={cn("h-6 w-6", className)}
+      {...props}
     >
       <path d="M12 2L2 7l10 5 10-5-10-5z" />
       <path d="M2 17l10 5 10-5" />
