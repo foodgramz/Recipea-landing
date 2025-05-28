e644b39823d95c757609d1cacb1640af001ef9b7
foodgramz <29430374@qq.com>
2025-05-28 22:00:46 +0800

    chore: comment out stripe integration

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,13 +1,13 @@
-import Stripe from 'stripe'
+// import Stripe from 'stripe'
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error('Missing STRIPE_SECRET_KEY')
-}
+// if (!process.env.STRIPE_SECRET_KEY) {
+//   throw new Error('Missing STRIPE_SECRET_KEY')
+// }
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2025-04-30.basil',
-  typescript: true,
-})
+// export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+//   apiVersion: '2025-04-30.basil',
+//   typescript: true,
+// })
 
 export const PLANS = {
   BASIC: {
